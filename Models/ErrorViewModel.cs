0bc423f36579a0619939c463e42b861ac5a1f5b3
Muhammad Hatta Aldino Ramadhan <muhammad.hatta@primavista-solusi.com>
2024-06-03 16:58:44 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FirstMVCAspnetApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
